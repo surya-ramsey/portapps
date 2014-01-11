82803990f2181971282d02f7440e15004819f976
Surya <surya.surakhman@wgs.co.id>
2014-01-11 19:39:33 +0700

    push app baru

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '859252f4c3a9eb505d352986534808abab57861b8dc0682b4b5baab6a44625bf1801b9f08c44662e75784a5ff881b8b6c9c95a9314e7fc70b190014abdb9f24e'
